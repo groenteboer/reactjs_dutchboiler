c57c6c21cd4ca06cf323aa65f136de6683f9af91
Dimitri Couwenberg <couwenberg@gmail.com>
2017-06-02 20:40:53 +1000

    added loader in webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require("path");
+module.exports = {
+    entry: {
+        app: ["./app/app.js"]
+    },
+    output: {
+        path: path.resolve(__dirname, "build"),
+        publicPath: "/assets/",
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+};
