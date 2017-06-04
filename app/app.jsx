3823db0debcc9f20e9fde9a2522f7b743624859f
Dimitri Couwenberg <couwenberg@gmail.com>
2017-06-04 15:09:56 +1000

    webpack started to change config

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-
 import {Hello} from './components/hello.jsx'; //todo remember to put component in curly brackets when importing
+
+//import './scss/app.scss';
 
 ReactDOM.render(<Hello timeofday="am"/> , document.getElementById('root'));
