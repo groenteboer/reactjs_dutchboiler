74dd1a1e1c37bdc122e3ed9bf9cbd62ace35e915
Dimitri Couwenberg <couwenberg@gmail.com>
2017-06-05 18:21:39 +1000

    css loader working

--- app/app.jsx
+++ app/app.jsx
@@ -2,7 +2,9 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import {Hello} from './components/hello.jsx'; //todo remember to put component in curly brackets when importing
 
-import './scss/app.scss';
+
+//todo get sass loader working
+//import './scss/app.scss';
 
 import './css/test.css';
 ReactDOM.render(<Hello timeofday="am"/> , document.getElementById('root'));
