0e6f0e3cc66a0b3c1edb85fc002eef161c95ef6a
Dimitri Couwenberg <couwenberg@gmail.com>
2017-06-06 19:28:24 +1000

    added kid and boiler

---
+++ app/components/kid.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Kid extends Component {
+    render() {
+        return (
+              <div className="stuff">
+                   some text : dont push me down
+               </div>
+        );
+    }
+}
+
+export default Kid;
