b78df911ed8b0cd4534c9850203b63e1e72265bf
Dimitri Couwenberg <couwenberg@gmail.com>
2017-06-06 15:59:08 +1000

    snippets installed

--- app/app.jsx
+++ app/app.jsx
@@ -1,10 +1,13 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import {Hello} from './components/hello.jsx'; //todo remember to put component in curly brackets when importing
-
+import {Hero} from './components/hero.jsx';
 
 //todo get sass loader working
 //import './scss/app.scss';
 
+//import 'bootstrap/dist/css/bootstrap.css';
+
 import './css/test.css';
-ReactDOM.render(<Hello timeofday="am"/> , document.getElementById('root'));
+
+ReactDOM.render(<Hero/>, document.getElementById('root'));
