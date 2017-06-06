9fa94da1492564201bdc045951436313c8081135
Dimitri Couwenberg <couwenberg@gmail.com>
2017-06-06 18:25:40 +1000

    error in css-loader

--- app/app.jsx
+++ app/app.jsx
@@ -3,12 +3,6 @@ import React from 'react';
 import {Hello} from './components/hello.jsx'; //todo remember to put component in curly brackets when importing
 import {Hero} from './components/hero.jsx';
 
-//todo get sass loader working
-//import './scss/app.scss';
-
-//import 'bootstrap/dist/css/bootstrap.css';
-
-
 import './css/bootstrap.css';
 
 ReactDOM.render(<Hero/>, document.getElementById('root'));
