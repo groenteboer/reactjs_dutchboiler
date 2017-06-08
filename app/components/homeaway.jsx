7304b22ecd306ea048342bf02a6f5a71d53b9f04
Dimitri Couwenberg <couwenberg@gmail.com>
2017-06-08 16:33:48 +1000

    update

--- app/components/homeaway.jsx
+++ app/components/homeaway.jsx
@@ -1,18 +1,36 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import styled from 'styled-components';
 
 export class HomeAway extends Component {
     render() {
+
+        const Div = styled.div`
+	border-radius: 3px;
+	padding: 0.25em 1em;
+	top:400px;
+	margin: 0 1em;
+	background: transparent;
+	opacity:
+	color: black;
+	border: 22px solid palevioletred;
+
+`
+
+
         return (
-            <div>
-                <img className="tileimage" src="./tile.jpg" alt=""/>
-                <div className="col-lg-12">
-                    <img className="tileseven" src="./logo.png" alt=""/>
+            <div className="container">
+                <img className="col-lg-12 tileimage" src="./tile.jpg" alt=""/>
+                <div className="col-lg-12 tileseven">
+                    <div className="col-md-3">
+                        <img className="" src="./logo.png" alt=""/>
+                    </div>
+                    <div className="col-md-9">
+                        <p>this is some text</p>
+                    </div>
                 </div>
-
-
             </div>
-        );
-    }
-}
+                );
+                }
+                }
 
-export default HomeAway;
+                export default HomeAway;
