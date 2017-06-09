321c21a7594df9a5e4bd55b76ac02427afb51170
Dimitri Couwenberg <couwenberg@gmail.com>
2017-06-09 21:08:36 +1000

    div in a div

--- app/components/divindiv.jsx
+++ app/components/divindiv.jsx
@@ -1,17 +1,18 @@
 import React, {Component} from 'react';
-import styled from 'styled-components';
 
 export class DivDiv extends Component {
     render() {
-
         return (
-            <div>
-lskfdjldsjflkdsjflkjsdlkj
-                <div className="row childDiv">
+            <div id="parentDiv">
+                <div className="col-md-12">
+                    {/*<img src="https://kbdevstorage1.blob.core.windows.net/asset-blobs/19230_en_1" alt=""/>*/}
+
+                </div>
+
+                <div className="row childDiv ">
                     <div className="col-md-6">one</div>
                     <div className="col-md-6">two</div>
                 </div>
-
             </div>
         );
     }
