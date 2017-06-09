d275bd76298010dd6213ded8aac3e99e8cc29c34
Dimitri Couwenberg <couwenberg@gmail.com>
2017-06-09 21:52:35 +1000

    added background css

--- app/components/divindiv.jsx
+++ app/components/divindiv.jsx
@@ -3,12 +3,8 @@ import React, {Component} from 'react';
 export class DivDiv extends Component {
     render() {
         return (
-            <div id="parentDiv">
-                <div className="col-md-12">
-                    {/*<img src="https://kbdevstorage1.blob.core.windows.net/asset-blobs/19230_en_1" alt=""/>*/}
 
-                </div>
-
+            <div id="parentDiv" className="bg">
                 <div className="row childDiv ">
                     <div className="col-md-6">one</div>
                     <div className="col-md-6">two</div>
