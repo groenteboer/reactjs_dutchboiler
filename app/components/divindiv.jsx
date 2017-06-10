8b455916d32c52d92692efd301b4f3236ea45adf
Dimitri Couwenberg <couwenberg@gmail.com>
2017-06-10 12:11:22 +1000

    css: responsive image background

--- app/components/divindiv.jsx
+++ app/components/divindiv.jsx
@@ -6,7 +6,8 @@ export class DivDiv extends Component {
 
             <div id="parentDiv" className="bg">
                 <div className="row childDiv ">
-                    <div className="col-md-6"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHvElQPvE4mpefl9D6qy2BmBsD2UCbKdJbn2I1kkpFRaTDoLhb2g" alt=""/></div>
+                    <div className="col-md-6">
+                        <img className="img-responsive" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHvElQPvE4mpefl9D6qy2BmBsD2UCbKdJbn2I1kkpFRaTDoLhb2g" alt=""/></div>
                     <div className="col-md-6">two</div>
                 </div>
             </div>
