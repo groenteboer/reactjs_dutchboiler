019082449ba971ba55ea509cbaf1f841977afd7c
Dimitri Couwenberg <couwenberg@gmail.com>
2017-06-12 19:05:36 +1000

    prop for array render

--- app/components/render_array.jsx
+++ app/components/render_array.jsx
@@ -8,24 +8,21 @@ export class Array_render extends Component {
     render() {
 
         //const j = Object.values(this.state.journal);
-        const j = [{'title':'one','date':'12/03/17','entry':'This is an entry'},
-            {'title':'two','date':'14/03/17','entry':'This is another entry'}
+        const j = [{'title': 'one', 'date': '12/03/17', 'entry': 'This is an entry'},
+            {'title': 'two', 'date': '14/03/17', 'entry': 'This is another entry'}
         ];
 
-        //inject j as property into Test
-        const Test = ({journals}) => (
-            <div>
-                {journals.map(journal => (
-                    <div>
-                        <h3>{journal.title} - {journal.date}</h3>
-                        <p>{journal.entry}</p>
-                    </div>
-                ))}
-            </div>
-        );
 
-        return (
-            <div><Test journals={j}></Test></div>
+        const Ding = //define property for variable
+        ({items}) => (
+                    <div>
+                        {items.map(j => (
+                            j.title
+                        ))}
+                    </div>
+                );
+
+        return (  <div><Ding items={j}/>  hello</div>
         );
     }
 
