254f9ba2080ffd53cb4344d9d46220b539064aa7
Dimitri Couwenberg <couwenberg@gmail.com>
2017-09-03 12:03:48 +1000

    updated

--- app/components/addproduct.js
+++ app/components/addproduct.js
@@ -2,26 +2,21 @@ import React, {Component} from 'react';
 
 export default class AddProduct extends Component {
 
-     constructor(props) {
-             super();
-             this.props = props;
-             this.state = {product:''}
-         }
-
+    constructor(props) {
+        super();
+        this.props = props;
+        this.state = {product: ''}
+    }
 
     handleAddproduct = () => {
-         
-        console.log('ref=',this.refs.name.value);
-
+        //does not get dispatched???:
         this.props.addProduct(this.refs.name.value);
-
-
     }
 
     render() {
-
         return (<div>
-                <input type="text" ref="name" /> <button onClick={this.handleAddproduct}>add product</button>
+                <input type="text" ref="name"/>
+                <button onClick={this.handleAddproduct}>add product</button>
             </div>
         );
     }
